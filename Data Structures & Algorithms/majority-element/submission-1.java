f2fdde9aa9248aba418f6612eb5849a26d77958b
Abhishek Kumar <k.abhishek06091998@gmail.com>
2026-08-30 01:41:17 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cnt=1;
+        int major =nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(major==nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+                if(cnt==0)
+                major=nums[i];
+            }
+        }
+        return major;
+    }
+}
